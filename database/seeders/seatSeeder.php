a67391fd7502c0729e889cc2f7120499f0ab023e
heddisdelviya <heddisdelviya@gmail.com>
2025-07-22 14:53:53 +0700

    perubahan halaman login

--- database/seeders/seatSeeder.php
+++ database/seeders/seatSeeder.php
@@ -9,16 +9,16 @@ class SeatSeeder extends Seeder
 {
     public function run(): void
     {
-        // Contoh menambahkan 10 seat untuk schedule ID 1
-<<<<<<< HEAD
-        // foreach (range(1, 10) as $i) {
-        //     Seat::create([
-        //         'schedule_id' => 1,
-        //         'seat_number' => 'A' . $i,
-        //         'status' => 'tersedia'
-        //     ]);
-        // }
-=======
+        //Contoh menambahkan 10 seat untuk schedule ID 1
+
+         foreach (range(1, 10) as $i) {
+             Seat::create([
+                 'schedule_id' => 1,
+                 'seat_number' => 'A' . $i,
+                 'status' => 'tersedia'
+           ]);
+         }
+
         foreach (range(1, 10) as $i) {
             Seat::create([
                 'schedule_id' => 2,
@@ -26,6 +26,6 @@ class SeatSeeder extends Seeder
                 'status' => 'tersedia'
             ]);
         }
->>>>>>> 4a9f8915d228f898db3df2e80e67a8800910b8b5
+
     }
 }
